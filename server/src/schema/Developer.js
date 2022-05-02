977de12b4d2c731747fb7fd0edef17db11fa5aa4
sharatd <sharat.lakers24@gmail.com>
2022-05-01 20:10:34 -0500

    schemas

--- server/src/schema/Developer.js
+++ server/src/schema/Developer.js
@@ -6,22 +6,44 @@ const DeveloperSchema = new Schema({
     type: Schema.Types.String,
     required: true
   },
-  languages: [{
+  technologies: [{
     name: Schema.Types.String,
     rating: Schema.Types.Number
   }],
-// Example of array
-//   rsvp: [{
-//     type: Schema.Types.ObjectId,
-//     ref: "Event",
-//     required: true
-//   }]
+  projects: [{
+    name: Schema.Types.String,
+    description: Schema.Types.String
+  }],
+  resume: {
+    type: Schema.Types.String,
+    required: false
+  },
+  availability: {
+    type: Schema.Types.Boolean,
+    required: true
+  },
+  topLanguage: {
+    type: Schema.Types.String,
+    required: true
+  },
+  timeCommitment: {
+    type: Schema.Types.Number,
+    required: true
+  },
+  preferredTopics: [Schema.Types.String],
+  school: {
+    type: Schema.Types.String,
+    required: true
+  },
+  preferredLanguages: [Schema.Types.String],
 });
 
 DeveloperSchema.statics.create = function(obj) {
   const Developer = mongoose.model("Developer", DeveloperSchema);
-  const developer = new User();
+  const developer = new Developer();
   developer.email = obj.email;
+  developer.technologies = obj.technologies;
+  developer.projects = obj.projects;
   return developer;
 }
 
