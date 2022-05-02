45ea62eaf93e8733fd22d6fce76fb54564479b52
sharatd <sharat.lakers24@gmail.com>
2022-05-01 21:44:50 -0500

    firebase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { useDevelopers } from "./utils/api";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import BrowseDevelopers from "./components/BrowseDevelopers";
 
 const App = () => {
-  const [developers, loading, error] = useDevelopers();
-
-  if (error) return <h1>{error}</h1>;
-  if (loading) return <h1>Loading the developers...</h1>
-
+ 
   return (
     <Router>
       <Switch>
         <Route path="/">
-          <h1>Xenah Dev Portal</h1>
-          <div>
-            {developers.map((developer, index) => (
-              <div style={{padding: "1em", border: "solid 1px black", margin: "1em"}} key={index}>
-                {JSON.stringify(developer)}
-              </div>
-            ))}
-          </div>
+          <BrowseDevelopers/>
         </Route>
       </Switch>
     </Router>
