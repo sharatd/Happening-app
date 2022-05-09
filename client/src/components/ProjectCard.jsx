1a10ba9379b246c22df66d73ae27a653d77c8787
[Quinton Nickum] <q.c.nickum@gmail.com>
2022-05-08 20:33:37 -0500

    Finished button styling

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -9,14 +9,17 @@ const ProjectCard = ({ project }) => {
           <Card style={{ margin: '1em', width: '90%'}}>
             <CardContent style={{ display: 'flex', flexDirection: 'column'}}>
                 <div style={{ display: 'flex', flexDirection: 'row'}}>
-                  <h3 style={{margin: '0'}}> { project.title } </h3>
-                  <Button style={{width: 'fit-content'}}>Edit</Button>
+                  <h3 style={{margin: '0', padding: '0.5em'}}> { project.title } </h3>
+                  <div style={{ marginLeft: 'auto', display: 'flex', flexDirection: 'row', gap: '5px'}}>
+                    <Button style={{width: 'fit-content', padding: '0.5em', color: ''}}>Applicants</Button>
+                    <Button style={{width: 'fit-content', padding: '0.5em'}}>Edit</Button>
+                    <Button style={{width: 'fit-content', padding: '0.5em', color: 'red'}}>X</Button>
+                  </div>
                 </div>
                 <div>
                     <p> Description: </p>
                     <p style={{marginLeft: '2em'}}> { project.description } </p>
                 </div>
-                
             </CardContent>
           </Card>
           
