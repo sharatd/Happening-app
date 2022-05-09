47fed86d6cfa6dc524bbb0eb70eda04cbcd5009c
yabsayele <yabiayele2023@u.northwestern.edu>
2022-05-08 20:14:46 -0500

    Editing project card

--- client/src/components/AdminProjectsView.jsx
+++ client/src/components/AdminProjectsView.jsx
@@ -3,9 +3,29 @@ import ProjectCard from "./ProjectCard"
 
 const AdminProjectsView = () => {
 
+    const projects = [
+        {
+            title: "Happening",
+            description: "Mobile app to view events on campus for Northwestern"
+        },
+        {
+            title: "Google Maps",
+            description: "Mobile app to view events in Evanston"
+        },
+        {
+            title: "Google Maps",
+            description: "Mobile app to view events in Evanston"
+        }
+    ]
+
     return(
         <div>
-            <ProjectCard title="Project Title" description="Description"></ProjectCard>
+            <h1 style={{ paddingLeft: '1.5em'}}>Projects</h1>
+            <div style={{display: 'flex', flexFlow: 'wrap', justifyContent: 'center'}}>
+                {projects.map((project, index) => (
+                <ProjectCard key={index} project={project}/>
+                ))}
+            </div>
         </div>
     )
 }
--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import Button from '@mui/material/Button'
 
-
-const ProjectCard = ({ title, description} ) => {
+const ProjectCard = ({ project }) => {
     return(
         <>
-          <Card style={{ margin: '1em', width: '20em' }}>
+          <Card style={{ margin: '1em', width: '90%'}}>
             <CardContent style={{ display: 'flex', flexDirection: 'column'}}>
-                <h3> { title } </h3>
+                <div style={{ display: 'flex', flexDirection: 'row'}}>
+                  <h3 style={{margin: '0'}}> { project.title } </h3>
+                  <Button style={{width: 'fit-content'}}>Edit</Button>
+                </div>
                 <div>
                     <p> Description: </p>
-                    <p> { description } </p>
+                    <p style={{marginLeft: '2em'}}> { project.description } </p>
                 </div>
+                
             </CardContent>
           </Card>
           
