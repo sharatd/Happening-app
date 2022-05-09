69c8f0b2f6224a8f1ee123de3034b2ecc4d89ed2
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2022-05-08 19:33:43 -0500

    Started dev modal styling

--- client/src/components/DeveloperInfo.jsx
+++ client/src/components/DeveloperInfo.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Chip from "@mui/material/Chip";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Avatar from '@mui/material/Avatar';
+import { deepPurple } from "@mui/material/colors";
 
-const DeveloperInfo = ({ developer }) => {
+const DeveloperInfo = ({ show, onClose, developer }) => {
   return (
-    <Card>
-      <CardContent>
-        <Typography variant={"h5"}>{developer.name}</Typography>
-        <Typography variant={"subtitle1"}>{developer.email}</Typography>
+    <Modal open={show} onClose={onClose}>
+      <Box 
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: '60%',
+          bgcolor: 'background.paper',
+          border: '2px solid #000',
+          boxShadow: 24,
+          p: 4,
+        }}
+      >
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          <div style={{width: 'auto' }}>
+            <Avatar
+              alt={developer.name + ' avatar'}
+              sx={{ bgcolor: deepPurple[500], height: 80, width: 80 }}
+            >
+              { developer.name.substr(0, 1)}
+            </Avatar>
+          </div>
+          <div style={{ marginLeft: '1em' }}>
+            <h2>{ developer.name }</h2>
+            <p>{ developer.email }</p>
+            <p>Available { developer.timeCommitment }hr/wk </p>
+          </div>
+        </div>
         <Typography>{developer.school}</Typography>
         <Typography>Languages:</Typography>
         {developer.technologies.sort((dev1, dev2) => dev2.rating - dev1.rating).map((technology, index) => (
@@ -34,8 +62,12 @@ const DeveloperInfo = ({ developer }) => {
         {developer.preferredLanguages.map((language, index) => (
           <Chip key={index} label={language}/>
         ))}
-      </CardContent>
-  </Card>
+
+        <div>
+          <Button onClick={onClose}>Close</Button>
+        </div>
+      </Box>
+    </Modal>
   );
 }
 
