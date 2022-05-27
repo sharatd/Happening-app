5b7eb4b26fae1615921610ac20d12233bcdc0133
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2022-05-26 21:28:33 -0500

    Initial framework for developer creation path

--- client/src/components/PrivateComponent.jsx
+++ client/src/components/PrivateComponent.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Switch, Route } from 'react-router-dom';
 import BrowseDevelopers from "./BrowseDevelopers";
 import AdminProjectsView from "./AdminProjectsView";
+import { useUserState } from '../utils/firebase';
+import { login } from '../utils/api';
 
 const PrivateComponent = () => {
+  const [user] = useUserState();
+  const [accountInfo, setAccountInfo] = useState('loading');
+
+  useEffect(() => {
+    const adminEmails = ['will@xenah.dev'];
+    if (adminEmails.includes(user.email)) {
+      setAccountInfo({ role: 'admin'});
+
+    } else {
+
+      login(user.email)
+        .then((devInfo) => {
+          console.log('devinfo', devInfo);
+          setAccountInfo(devInfo);
+        });
+    }
+  }, [user]);
+
+  if (accountInfo === 'loading')
+    return <h1>Loading...</h1>;
+
+  if (!accountInfo) {
+    // Show sign-up form
+    // Have callback to setAccountInfo when submitting form
+    return <h1>Dev sign up form...</h1>
+  }
 
   return (
     <Switch>
